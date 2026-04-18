5f186e4298434020260b1057eac6221aacce3518
Yajat-kanaskar05 <yajat.important@gmail.com>
2026-04-18 20:39:16 +0530

    Another question solved

---
+++ Number_of_1_bit.py
@@ -0,0 +1,16 @@
+"""
+problem - Number of one bit
+problem link - https://leetcode.com/problems/number-of-1-bits/description/
+"""
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        
+        count = 0
+
+        while(n != 0):
+            if(n & 1):
+                count += 1
+
+            n = n >> 1
+        return count
